d4ea3bbe28921b68258967822d86cb9ff6df137a
Logan HAUSPIE <logan.hauspie.pro@gmail.com>
2017-07-26 09:52:12 +0200

    rename subfolders

--- nightclazz-spring-webflux/src/main/kotlin/com/github/lhauspie/nightclazz/spring/webflux/config/MyDrawing.kt
+++
@@ -1,4 +0,0 @@
-package com.github.lhauspie.nightclazz.spring.webflux.config
-
-data class MyDrawing (val author: String) {
-}
--- nightclazz-spring-webflux/src/main/kotlin/com/github/lhauspie/nightclazz/spring/webflux/config/RouterConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.github.lhauspie.nightclazz.spring.webflux.config
-
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-class RouterConfigg {
-
-}
